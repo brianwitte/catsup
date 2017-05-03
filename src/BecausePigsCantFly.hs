dadb36687040cc80f5eb2e8533d61df02a62a568
bwiyy <openbw@protonmail.com>
2017-05-03 01:00:58 -0500

    some more early exposure to types (Bool)

--- src/BecausePigsCantFly.hs
+++ src/BecausePigsCantFly.hs
@@ -12,6 +12,12 @@ allAwesome = [awesome, alsoAwesome]
 showLengthOfAllAwesome :: Int
 showLengthOfAllAwesome = length allAwesome
 
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs x = if (x < 0) then 0 - x else x
+
 main :: IO ()
 main = print $ length allAwesome
 
