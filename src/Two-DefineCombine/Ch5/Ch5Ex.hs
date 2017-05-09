79423d6a0d1c99bf198929338624d1bc8a321dc8
Witte, Brian <brian.witte@accenture.com>
2017-05-09 13:19:00 -0500

    finished Type ex & reformatted scaffolding

--- src/Two-DefineCombine/Ch5/Ch5Ex.hs
+++ src/Two-DefineCombine/Ch5/Ch5Ex.hs
@@ -85,5 +85,67 @@ sing = if (x > y) then fstString x else sndString y
 
 -- skip numba 2
 
--- TODO: number 3
+-- number 3 is main func below
 
+-- Type-Kwon-Do
+
+-- Example
+data Woot
+data Blah 
+
+fTypeKwon :: Woot -> Blah
+fTypeKwon = undefined
+
+gTypeKwon :: (Blah, Woot) -> (Blah, Blah)
+gTypeKwon (x, y) = (x, fTypeKwon y)
+
+-- question 1
+fDoe1 :: Int -> String 
+fDoe1 = undefined
+
+gDoe1 :: String -> Char
+gDoe1 = undefined
+
+hDoe1 :: Int -> Char
+hDoe1 x = gDoe1 $ fDoe1 x 
+
+-- question 2
+data A 
+data B 
+data C 
+
+qDoe2 :: A -> B 
+qDoe2 = undefined
+
+wDoe2 :: B -> C 
+wDoe2 = undefined
+
+eDoe2 :: A -> C 
+eDoe2 x = wDoe2 $ qDoe2 x
+
+-- question 3
+data X 
+data Y 
+data Z
+
+xzDoe :: X -> Z
+xzDoe = undefined
+
+yzDoe :: Y -> Z
+yzDoe = undefined
+
+xForm :: (X, Y) -> (Z, Z)
+xForm (x, y) = (xzDoe x, yzDoe y)
+
+-- question 4
+munge :: (x -> y) -> (y -> (w,z)) -> x -> w 
+munge xy ywz x = fst $ ywz $ xy x
+
+-- the main func is number 3 from "Fix it" section
+main :: IO()
+main = do
+  print $ 1 + 2
+  print 10
+  print $ negate (-1)
+  print $ (+) 0 blah
+    where blah = negate 1
