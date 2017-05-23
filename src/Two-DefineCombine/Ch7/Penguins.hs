c1da0d55ccaa29e5245408ea3fd01bfd37437b1e
bwiyy <bwiyy@users.noreply.github.com>
2017-05-22 22:55:11 -0500

    refactoring and added Penguin file

---
+++ src/Two-DefineCombine/Ch7/Penguins.hs
@@ -0,0 +1,28 @@
+module Penguins where
+
+data WherePenguinsLive =
+    Galapagos
+  | Antarctica
+  | Australia
+  | SouthAfrica
+  | SouthAmerica
+  deriving (Eq, Show)
+
+data Penguin =
+  Peng WherePenguinsLive
+  deriving (Eq, Show)
+
+isSouthAfrica :: WherePenguinsLive -> Bool
+isSouthAfrica SouthAfrica = True
+isSouthAfrica _           = False
+
+gimmeWhereTheyLive :: Penguin -> WherePenguinsLive
+gimmeWhereTheyLive (Peng whereitlives) = whereitlives
+
+-- TEST DATA
+
+humboldt = Peng SouthAmerica
+gentoo = Peng Antarctica
+macaroni = Peng Antarctica
+little = Peng Australia
+galapagos = Peng Galapagos
