2d4b427be811693eed230a4a8520c91b08b79216
bwiyy <bwiyy@users.noreply.github.com>
2017-05-25 18:00:37 -0500

    starting recursion

---
+++ src/Two-DefineCombine/Ch8/Factorial.hs
@@ -0,0 +1,9 @@
+module Factorial where
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+incTimes :: (Eq a, Num a) => a -> a -> a
+incTimes 0 n = n
+incTimes times n = 1 + (incTimes (times - 1) n)
