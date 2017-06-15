08f118a33897e02858809ae3d79ad74308c0cb00
bwiyy <bwiyy@users.noreply.github.com>
2017-06-15 00:43:14 -0500

    using Maybe for exceptions

--- src/Two-DefineCombine/Ch9/StartList.hs
+++ src/Two-DefineCombine/Ch9/StartList.hs
@@ -1,5 +1,14 @@
 module StartList where
 
+
+-- No exception handling in these funcs
 myHead (x : _) = x
 
 myTail (_ : xs) = xs
+
+
+-- This one does using Maybe
+safeTail :: [a] -> Maybe [a]
+safeTail []     = Nothing
+safeTail (x:[]) = Nothing
+safeTail (_:xs) = Just xs
