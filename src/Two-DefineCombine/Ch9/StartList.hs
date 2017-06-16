8036e2126daed677635830e4595f3a0753a95230
bwiyy <bwiyy@users.noreply.github.com>
2017-06-16 10:35:04 -0500

    let's see here merge

--- src/Two-DefineCombine/Ch9/StartList.hs
+++ src/Two-DefineCombine/Ch9/StartList.hs
@@ -2,9 +2,9 @@ module StartList where
 
 
 -- No exception handling in these funcs
-myHead (x : _) = x
+myHead (x:_) = x
 
-myTail (_ : xs) = xs
+myTail (_:xs) = xs
 
 
 -- This one does using Maybe
