bb19774ed2f3ffb917bd334edee7c5ae89b88823
buoy49 <buoy5000@gmail.com>
2019-03-04 19:11:28 -0600

    ch10 continued

--- src/Two-DefineCombine/Ch10/DbFolds.hs
+++ src/Two-DefineCombine/Ch10/DbFolds.hs
@@ -13,6 +13,8 @@ theDatabase =
             (fromGregorian 1911 5 1)
     (secondsToDiffTime 34123))
   , DbNumber 9001
+  , DbNumber 9001
+  , DbNumber 9001
   , DbString "Hello, world!"
   , DbDate (UTCTime
             (fromGregorian 1921 5 1)
@@ -20,4 +22,31 @@ theDatabase =
   ]
 
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate (x:xs) = undefined
+filterDbDate d = foldr helper [] d
+  where helper (DbDate a) b = a : b
+        helper _ b = b
+
+filterDbDate' :: [DatabaseItem] -> [UTCTime]
+filterDbDate' d = foldr (\a b -> case a of
+                            (DbDate a) -> a : b
+                            (_) -> b) [] d
+
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber d = foldr (\a b -> case a of
+                            (DbNumber a) -> a : b
+                            (_) -> b) [] d
+
+filterDbString :: [DatabaseItem] -> [String]
+filterDbString d = foldr (\a b -> case a of
+                            (DbString a) -> a : b
+                            (_) -> b) [] d
+
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent d = maximum $ filterDbDate d
+
+sumDb :: [DatabaseItem] -> Integer
+sumDb d = sum $ filterDbNumber d
+
+avgDb :: [DatabaseItem] -> Double
+avgDb d = fromIntegral (sumDb d) / fromIntegral (length $ filterDbNumber d)
+
